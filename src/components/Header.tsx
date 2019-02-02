8796df45fb1df9044e98a043b72015e30ed6d8af
Jason E. Rist <jrist@redhat.com>
2019-02-01 17:11:08 -0700

    Add MetalKube Facet logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Avatar, Brand, PageHeader } from '@patternfly/react-core';
 import avatarImg from '@patternfly/patternfly-next/assets/images/img_avatar.svg';
-import brandImg from '@patternfly/patternfly-next/assets/images/pf_mini_logo_white.svg';
+import logo from '../images/logo.svg';
+
 
 const Header: React.FunctionComponent = (): React.ReactElement<
   React.ReactNode
 > => (
   <PageHeader
-    logo={<Brand src={brandImg} alt="RHHI.Next logo" />}
+    logo={<Brand src={logo} alt="RHHI.Next logo" />}
     // toolbar={PageToolbar}
     avatar={<Avatar src={avatarImg} alt="Avatar image" />}
     // showNavToggle
