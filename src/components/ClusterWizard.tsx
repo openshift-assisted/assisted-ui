ef7b2cb3d877ff05b8b96d82b869ffe1d6bcca15
Jiri Tomasek <jtomasek@redhat.com>
2019-02-02 16:31:48 +0100

    ClusterWizard components and custom PageSection

--- src/components/ClusterWizard.tsx
+++ src/components/ClusterWizard.tsx
@@ -1,136 +1,17 @@
 import React, { FC, Fragment } from 'react';
-import {
-  Button,
-  PageSection,
-  PageSectionVariants,
-  StackItem,
-  TextContent,
-  Text
-} from '@patternfly/react-core';
 
-interface ClusterWizardProps {
-  name?: string;
-}
+import BaremetalInventory from './BaremetalInventory';
+import ClusterWizardNav from './ClusterWizardNav';
+import ClusterWizardSteps from './ClusterWizardSteps';
 
-const ClusterWizard: FC<ClusterWizardProps> = ({
-  name = 'New Cluster'
-}: ClusterWizardProps): JSX.Element => (
+const ClusterWizard: FC = (): JSX.Element => (
   <Fragment>
-    <StackItem isMain={false}>
-      <PageSection variant={PageSectionVariants.light}>
-        <TextContent>
-          <Text component="h1">Deploy Bare Metal Hyper Converged Cluster</Text>
-          <Text component="p">Wizard steps go here</Text>
-        </TextContent>
-      </PageSection>
-    </StackItem>
-    <StackItem isMain style={{ position: 'relative' }}>
-      <PageSection
-        style={{
-          position: 'absolute',
-          height: '100%',
-          width: '100%',
-          overflow: 'auto',
-          zIndex: 0
-        }}
-        variant={PageSectionVariants.default}
-      >
-        <TextContent>
-          <Text component="h2" style={{ position: 'sticky', top: -32 }}>
-            {name} Baremetal Node Inventory
-          </Text>
-          <Text component="p">
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn't anything embarrassing hidden in the
-            middle of text. All the Lorem Ipsum generators on the Internet tend
-            to repeat predefined chunks as necessary, making this the first true
-            generator on the Internet. It uses a dictionary of over 200 Latin
-            words, combined with a handful of model sentence structures, to
-            generate Lorem Ipsum which looks reasonable. The generated Lorem
-            Ipsum is therefore always free from repetition, injected humour, or
-            non-characteristic words etc.
-          </Text>
-          <Text component="p">
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn't anything embarrassing hidden in the
-            middle of text. All the Lorem Ipsum generators on the Internet tend
-            to repeat predefined chunks as necessary, making this the first true
-            generator on the Internet. It uses a dictionary of over 200 Latin
-            words, combined with a handful of model sentence structures, to
-            generate Lorem Ipsum which looks reasonable. The generated Lorem
-            Ipsum is therefore always free from repetition, injected humour, or
-            non-characteristic words etc.
-          </Text>
-          <Text component="p">
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn't anything embarrassing hidden in the
-            middle of text. All the Lorem Ipsum generators on the Internet tend
-            to repeat predefined chunks as necessary, making this the first true
-            generator on the Internet. It uses a dictionary of over 200 Latin
-            words, combined with a handful of model sentence structures, to
-            generate Lorem Ipsum which looks reasonable. The generated Lorem
-            Ipsum is therefore always free from repetition, injected humour, or
-            non-characteristic words etc.
-          </Text>
-          <Text component="p">
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn't anything embarrassing hidden in the
-            middle of text. All the Lorem Ipsum generators on the Internet tend
-            to repeat predefined chunks as necessary, making this the first true
-            generator on the Internet. It uses a dictionary of over 200 Latin
-            words, combined with a handful of model sentence structures, to
-            generate Lorem Ipsum which looks reasonable. The generated Lorem
-            Ipsum is therefore always free from repetition, injected humour, or
-            non-characteristic words etc.
-          </Text>
-          <Text component="p">
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn't anything embarrassing hidden in the
-            middle of text. All the Lorem Ipsum generators on the Internet tend
-            to repeat predefined chunks as necessary, making this the first true
-            generator on the Internet. It uses a dictionary of over 200 Latin
-            words, combined with a handful of model sentence structures, to
-            generate Lorem Ipsum which looks reasonable. The generated Lorem
-            Ipsum is therefore always free from repetition, injected humour, or
-            non-characteristic words etc.
-          </Text>
-          <Text component="p">
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn't anything embarrassing hidden in the
-            middle of text. All the Lorem Ipsum generators on the Internet tend
-            to repeat predefined chunks as necessary, making this the first true
-            generator on the Internet. It uses a dictionary of over 200 Latin
-            words, combined with a handful of model sentence structures, to
-            generate Lorem Ipsum which looks reasonable. The generated Lorem
-            Ipsum is therefore always free from repetition, injected humour, or
-            non-characteristic words etc.
-          </Text>
-        </TextContent>
-      </PageSection>
-    </StackItem>
-    <StackItem isMain={false}>
-      <PageSection variant={PageSectionVariants.light}>
-        <Button variant="primary">Next</Button>
-      </PageSection>
-    </StackItem>
+    <ClusterWizardSteps
+      steps={[{ title: 'First' }, { title: 'Second' }]}
+      currentStepIndex={0}
+    />
+    <BaremetalInventory />
+    <ClusterWizardNav />
   </Fragment>
 );
 
---
+++ src/components/ui/PageSection.tsx
@@ -0,0 +1,33 @@
+import React, { FC, CSSProperties } from 'react';
+import {
+  PageSection as PfPageSection,
+  StackItem
+} from '@patternfly/react-core';
+
+interface PageSectionProps extends React.ComponentProps<typeof PfPageSection> {
+  isMain?: boolean;
+  style?: CSSProperties;
+}
+
+const scrollableStyle: CSSProperties = {
+  position: 'absolute',
+  height: '100%',
+  width: '100%',
+  overflow: 'auto',
+  zIndex: 0
+};
+
+const PageSection: FC<PageSectionProps> = ({
+  isMain = false,
+  style,
+  ...rest
+}: PageSectionProps): JSX.Element => {
+  const resultStyle = isMain ? { ...style, ...scrollableStyle } : { ...style };
+  return (
+    <StackItem isMain={isMain} style={{ position: 'relative' }}>
+      <PfPageSection style={resultStyle} {...rest} />
+    </StackItem>
+  );
+};
+
+export default PageSection;
