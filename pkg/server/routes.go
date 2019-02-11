1c91f45fd0e77e909422c83cb5115f099eb48e87
Honza Pokorny <honza@redhat.com>
2019-02-11 06:35:57 -0400

    Add long-running task infrastructure

--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -16,9 +16,10 @@ package server
 
 import (
 	"encoding/json"
-	"fmt"
+	"github.com/satori/go.uuid"
 	"net"
 	"net/http"
+	"time"
 )
 
 type Host struct {
@@ -34,11 +35,45 @@ type ApiResponse struct {
 	Data interface{} `json:"data"`
 }
 
-func respondWithJson(w http.ResponseWriter, obj interface{}) {
-	resp := ApiResponse{Data: obj}
-	err := json.NewEncoder(w).Encode(resp)
-	if err != nil {
-		fmt.Fprint(w, err)
+type NotificationStatus int
+
+const (
+	SUCCESS NotificationStatus = 0
+	RUNNING NotificationStatus = 1
+	FAILURE NotificationStatus = 2
+)
+
+func (ns NotificationStatus) String() string {
+	values := [...]string{
+		"SUCCESS",
+		"RUNNING",
+		"FAILURE",
+	}
+	if ns < SUCCESS || ns > FAILURE {
+		return "Unknown"
+	}
+
+	return values[ns]
+}
+
+func (ns NotificationStatus) MarshalJSON() ([]byte, error) {
+	return json.Marshal(ns.String())
+}
+
+type Notification struct {
+	Id        string             `json:"id"`
+	Message   string             `json:"message"`
+	Status    NotificationStatus `json:"status"`
+	Timestamp time.Time          `json:"timestamp"`
+}
+
+func NewNotification(message string, status NotificationStatus) Notification {
+	uuid, _ := uuid.NewV4()
+	return Notification{
+		Id:        uuid.String(),
+		Message:   message,
+		Status:    status,
+		Timestamp: time.Now(),
 	}
 }
 
@@ -62,4 +97,38 @@ func HostsHandler(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 	respondWithJson(w, hostList)
+}
+
+// This is an example of a task that might take a while to finish.  It takes a
+// Notification channel as an argument.  You can use this channel to send
+// Notification messages to the browser.
+func performLongTask(notificationChannel chan Notification) {
+	n := NewNotification("Started a long running task", RUNNING)
+	notificationChannel <- n
+
+	// Do important work for 10 seconds
+	time.Sleep(10 * time.Second)
+
+	n2 := NewNotification("Finished a long running task", SUCCESS)
+	notificationChannel <- n2
+}
+
+// This is an example of a REST API endpoint handler which will trigger a long
+// running task.  It takes a Notification channel as an argument, and returns a
+// standard HTTP handler.  The idea is to create a closure over the notification
+// channel.  When registering your handler with the router, you might do
+// something like:
+//
+//   router.HandleFunc("/some-endpoint", LongRunningTaskHandler(notificationChannel))
+//
+//  See pkg/server/server.go for an example.
+//
+// In the body itself, you are expected to start the actual long-running task in
+// a go routine, and quickly respond with a 2xx.
+func LongRunningTaskHandler(notificationChannel chan Notification) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		response := "OK"
+		go performLongTask(notificationChannel)
+		respondWithJson(w, response)
+	}
 }
