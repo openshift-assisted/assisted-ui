d5d184e1dd26f0fdfa69ff5589251e2472d80e7d
Honza Pokorny <honza@redhat.com>
2019-02-12 11:53:14 -0400

    Remove TODO

--- src/api/hosts.ts
+++ src/api/hosts.ts
@@ -4,5 +4,4 @@ import { ApiResponse } from './index';
 
 type HostListApiReponse = AxiosPromise<ApiResponse<Host[]>>;
 
-// TODO: Add configuration for the API endpoint
 export const getHosts = (): HostListApiReponse => axios.get('/api/hosts');
