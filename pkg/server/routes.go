0535078867b44647ebf21d4faaff78646a0b70db
Honza Pokorny <honza@redhat.com>
2019-02-13 11:32:36 -0400

    Add a REST API endpoint for trigger bootstrap vm boot

--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -26,6 +26,7 @@ func ApiRouter(router *mux.Router, notificationChannel chan common.Notification)
 
 	router.HandleFunc("/hosts", HostsHandler)
 	router.HandleFunc("/long", LongRunningTaskHandler(notificationChannel))
+	router.HandleFunc("/bootstrap-vm", BootstrapVMHandler(notificationChannel))
 }
 
 func CreateRouter(notificationChannel chan common.Notification, websocketWorker *WebsocketWorker) *mux.Router {
