4a67a92bbcbcc91074eecb33afb6683b36ffc554
Honza Pokorny <honza@redhat.com>
2019-02-14 14:09:36 -0400

    Fix lint errors in websocket services

--- src/api/websocket.ts
+++ src/api/websocket.ts
@@ -1,22 +1,22 @@
+// TODO: This is just a dumb client to test the websocket connection
 // TODO: This needs to be connected to the redux store
 
-const startWebsocketService = () => {
+const startWebsocketService = (): void => {
+  window.addEventListener('load', () => {
+    let ws = new WebSocket('ws://localhost:8080/ws');
 
-    window.addEventListener('load', () => {
-        let ws = new WebSocket('ws://localhost:8080/ws');
-
-        ws.addEventListener('message', event => {
-            try {
-                let message = JSON.parse(event.data);
-                console.log('Websocket message', message)
-            } catch(e) {
-                console.log(e)
-                return
-            }
-
-        });
+    ws.addEventListener('message', event => {
+      try {
+        let message = JSON.parse(event.data);
+        // eslint-disable-next-line no-console
+        console.log('Websocket message', message);
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e);
+        return;
+      }
     });
-
+  });
 };
 
 export default startWebsocketService;
