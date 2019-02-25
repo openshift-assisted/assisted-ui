9b8ad8a2a103b40c5859e009b80606d539a588ac
Honza Pokorny <honza@redhat.com>
2019-02-25 11:50:16 -0400

    Disable some eslint rules for console code
    The openshift/console project has softer eslint rules than us.

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,6 @@
+/* eslint @typescript-eslint/no-explicit-any: 0 */
+/* eslint @typescript-eslint/indent: 0 */
+
 // NOTE(honza): These types are copied and pasted from openshift/console
 
 export type MatchExpression =
