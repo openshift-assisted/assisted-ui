6c3896f842fc80969bba293f36f3fdcf2515dee7
Beth Elwell <belwell@redhat.com>
2019-02-27 13:15:01 +0000

    WIP Create selector to agregate values from all hosts

--- src/selectors/hosts.ts
+++ src/selectors/hosts.ts
@@ -11,3 +11,19 @@ export const getHostTableRows = createSelector(
   getHosts,
   hosts => hosts.map(host => Object.values(host))
 );
+
+const hostsValuesSelector = state => state.hosts.items;
+
+export const hostsAggregateValues = createSelector(
+  hostsValuesSelector,
+  items => items.reduce((acc, item) => acc + item.memory, 0)
+);
+
+let exampleState = {
+  hosts: {
+    memory: 8,
+    items: [{ name: 'test1', memory: 3 }, { name: 'test2', memory: 4 }]
+  }
+};
+
+console.log(hostsAggregateValues(exampleState));
