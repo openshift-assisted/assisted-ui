1b12edaa3cb48293e01f2778c2513983818ca74e
Jiri Tomasek <jtomasek@redhat.com>
2020-03-30 20:24:22 +0200

    initially create clusters with random name

--- src/components/clusters/NewCluster.tsx
+++ src/components/clusters/NewCluster.tsx
@@ -1,10 +1,18 @@
 import React, { useEffect } from 'react';
 import { Redirect, Link } from 'react-router-dom';
 import { PageSectionVariants, ButtonVariant, Button } from '@patternfly/react-core';
+import { uniqueNamesGenerator, Config, adjectives, colors, animals } from 'unique-names-generator';
 import PageSection from '../ui/PageSection';
 import { ErrorState, LoadingState } from '../ui/uiState';
 import { createCluster } from '../../api/clusters';
 import { Cluster } from '../../api/types';
+
+const namesConfig: Config = {
+  dictionaries: [adjectives, colors, animals],
+  separator: '-',
+  length: 3,
+  style: 'lowerCase',
+};
 
 const NewCluster: React.FC = () => {
   const [uiState, setUiState] = React.useState('loading');
@@ -13,12 +21,14 @@ const NewCluster: React.FC = () => {
   const createClusterAsync = async () => {
     try {
       setUiState('loading');
-      const { data } = await createCluster({ name: 'draft' });
+      const name: string = uniqueNamesGenerator(namesConfig);
+      const { data } = await createCluster({ name });
       setCluster(data);
       setUiState('done');
     } catch (e) {
       setUiState('error');
-      console.error(e.response.data);
+      console.error(e);
+      console.error(e.response?.data);
     }
   };
 
@@ -28,6 +38,7 @@ const NewCluster: React.FC = () => {
 
   const cancel = (
     <Button
+      key="cancel"
       variant={ButtonVariant.secondary}
       component={(props) => <Link to="/clusters" {...props} />}
     >
