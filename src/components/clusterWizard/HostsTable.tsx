3c06e2e9a9c6ce01de46773194531bc7bb34c781
Marek Libra <marek.libra@gmail.com>
2020-04-22 15:16:52 +0200

    Refactor HostsTable

--- src/components/clusterWizard/HostsTable.tsx
+++ src/components/clusterWizard/HostsTable.tsx
@@ -23,6 +23,67 @@ type Props = {
   variant?: TableVariant;
 };
 
+type HostRowHwInfo = {
+  cpu: string;
+  memory: string;
+  disk: string;
+};
+
+const getHostRowHardwareInfo = (hwInfoString: string): HostRowHwInfo => {
+  let hwInfo: Introspection = {};
+  try {
+    hwInfo = JSON.parse(hwInfoString);
+  } catch (e) {
+    console.error('Failed to parse Hardware Info', e);
+  }
+  return {
+    cpu: `${hwInfo?.cpu?.cpus}x ${Humanize.formatNumber(hwInfo?.cpu?.['cpu-mhz'] || 0)} MHz`,
+    memory: Humanize.fileSize(hwInfo?.memory?.[0]?.total || 0),
+    disk: Humanize.fileSize(
+      hwInfo?.['block-devices']
+        ?.filter((device: BlockDevice) => device['device-type'] === 'disk')
+        .reduce((diskSize: number, device: BlockDevice) => diskSize + (device?.size || 0), 0) || 0,
+    ),
+  };
+};
+
+const hostToHostTableRow = (host: Host): IRow => {
+  // console.log('--- host: ', host);
+  const { id, status, statusInfo, hardwareInfo = '' } = host;
+  const { cpu, memory, disk } = getHostRowHardwareInfo(hardwareInfo);
+  return {
+    // isOpen: true,
+    cells: [
+      id, // TODO: should be "name"
+      'Master', // TODO: should be flexible (a dropdown for master/worker)
+      id, // TODO: should be serial number
+      { title: <HostStatus status={status} statusInfo={statusInfo} /> },
+      cpu,
+      memory,
+      disk,
+    ],
+  };
+};
+
+const HostsTableEmptyState: React.FC = () => (
+  <EmptyState
+    icon={ConnectedIcon}
+    title="Waiting for hosts..."
+    content="Boot the discovery ISO on a hardware that should become part of this bare metal cluster. After booting the ISO the hosts get inspected and register to the cluster. At least 3 bare metal hosts are required to form the cluster."
+    primaryAction={<DiscoveryImageModalButton />}
+  />
+);
+
+const columns = [
+  { title: 'ID', cellFormatters: [expandable] },
+  { title: 'Role' },
+  { title: 'Serial Number' },
+  { title: 'Status' },
+  { title: 'vCPU' },
+  { title: 'Memory' },
+  { title: 'Disk' },
+];
+
 const HostsTable: React.FC<Props> = ({ hosts = [], uiState, fetchHosts, variant }) => {
   // const headerStyle = {
   //   position: 'sticky',
@@ -33,73 +94,16 @@ const HostsTable: React.FC<Props> = ({ hosts = [], uiState, fetchHosts, variant 
   // const headerStyle = {};
   // const headerConfig = { header: { props: { style: headerStyle } } };
 
-  const columns = [
-    { title: 'ID', cellFormatters: [expandable] },
-    { title: 'Role' },
-    { title: 'Serial Number' },
-    { title: 'Status' },
-    { title: 'vCPU' },
-    { title: 'Memory' },
-    { title: 'Disk' },
-  ];
-
-  type HostRowHwInfo = { cpu: string; memory: string; disk: string };
-
-  const getHostRowHardwareInfo = (hwInfoString: string): HostRowHwInfo => {
-    let hwInfo: Introspection = {};
-    try {
-      hwInfo = JSON.parse(hwInfoString);
-    } catch (e) {
-      console.error('Failed to parse Hardware Info', e);
-    }
-    return {
-      cpu: `${hwInfo?.cpu?.cpus}x ${Humanize.formatNumber(hwInfo?.cpu?.['cpu-mhz'] || 0)} MHz`,
-      memory: Humanize.fileSize(hwInfo?.memory?.[0]?.total || 0),
-      disk: Humanize.fileSize(
-        hwInfo?.['block-devices']
-          ?.filter((device: BlockDevice) => device['device-type'] === 'disk')
-          .reduce((diskSize: number, device: BlockDevice) => diskSize + (device?.size || 0), 0) ||
-          0,
-      ),
-    };
-  };
-
-  const hostToHostTableRow = (host: Host): IRow => {
-    // console.log('--- host: ', host);
-    const { id, status, statusInfo, hardwareInfo = '' } = host;
-    const { cpu, memory, disk } = getHostRowHardwareInfo(hardwareInfo);
-    return {
-      // isOpen: true,
-      cells: [
-        id, // TODO: should be "name"
-        'Master', // TODO: should be flexible (a dropdown for master/worker)
-        id, // TODO: should be serial number
-        { title: <HostStatus status={status} statusInfo={statusInfo} /> },
-        cpu,
-        memory,
-        disk,
-      ],
-    };
-  };
-
-  const hostRows = hosts.map(hostToHostTableRow);
   // const hostRows = hosts.map(hostToHostTableRow).reduce((newRows: IRow[], currentRow) => {
   //   newRows.push(currentRow);
   //   newRows.push({ parent: currentRow[0], fullWidth: true, cells: ['hello'] } as IRow);
   //   return newRows;
   // }, []);
 
-  const emptyState = (
-    <EmptyState
-      icon={ConnectedIcon}
-      title="Waiting for hosts..."
-      content="Boot the discovery ISO on a hardware that should become part of this bare metal cluster. After booting the ISO the hosts get inspected and register to the cluster. At least 3 bare metal hosts are required to form the cluster."
-      primaryAction={<DiscoveryImageModalButton />}
-    />
-  );
-  const errorState = <ErrorState title="Failed to fetch hosts" fetchData={fetchHosts} />;
+  const rows = React.useMemo(() => {
+    const errorState = <ErrorState title="Failed to fetch hosts" fetchData={fetchHosts} />;
+    const hostRows = hosts.map(hostToHostTableRow);
 
-  const getRows = () => {
     const columnCount = columns.length;
     switch (uiState) {
       // case ResourceUIState.LOADING:
@@ -112,11 +116,10 @@ const HostsTable: React.FC<Props> = ({ hosts = [], uiState, fetchHosts, variant 
         if (hostRows.length) {
           return hostRows;
         }
-        return getColSpanRow(emptyState, columnCount);
+        return getColSpanRow(HostsTableEmptyState, columnCount);
     }
-  };
+  }, [uiState, fetchHosts, hosts]);
 
-  const rows = getRows();
   return (
     <Table
       rows={rows}
