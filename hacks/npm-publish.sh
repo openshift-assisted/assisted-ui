28a17206584c7b242dd4b5815256f5e35c8537f9
Marek Libra <marek.libra@gmail.com>
2020-05-07 15:45:18 +0200

    Add npm-publish.sh script
    Intended to be manually executed to
    - increase package version
    - and publish to NPM registry

---
+++ hacks/npm-publish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+
+export TMPDIR=`mktemp -d /tmp/XXXX`
+cd ${TMPDIR}
+
+git clone https://github.com/openshift-metal3/facet.git
+cd facet
+pwd
+
+yarn version --patch
+export VERSION=`git log -1 --pretty=%B`
+
+export BRANCH=rel-${VERSION}
+git checkout -b ${BRANCH}
+git push --set-upstream origin ${BRANCH} 
+git push --tags
+xdg-open https://github.com/openshift-metal3/facet/compare/${BRANCH}?expand=1
+
+yarn publish --non-interactive --access public
+
