b6cab69cd063dac6a65003712f60d120696ac31e
Marek Libra <marek.libra@gmail.com>
2020-05-13 13:24:40 +0200

    Fix ClusterCreateParams.openshiftVersion
    Adapt to latest API change.

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,7 +1,14 @@
-export const OPENSHIFT_VERSION_OPTIONS = [
+import { ClusterCreateParams } from '../api/types';
+
+type OpenshiftVersionOptionType = {
+  label: string;
+  value: ClusterCreateParams['openshiftVersion'];
+};
+
+export const OPENSHIFT_VERSION_OPTIONS: OpenshiftVersionOptionType[] = [
   { label: 'OpenShift 4.4', value: '4.4' },
-  { label: 'OpenShift 4.5', value: '4.5' },
-  { label: 'OpenShift 4.6', value: '4.6' },
+  // { label: 'OpenShift 4.5', value: '4.5' },
+  // { label: 'OpenShift 4.6', value: '4.6' },
 ];
 
 export const CLUSTER_MANAGER_SITE_LINK =
