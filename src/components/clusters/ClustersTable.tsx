9a8f12cd5727ba5f106006b4919f40e5f65f9ee9
Marek Libra <marek.libra@gmail.com>
2020-05-13 16:05:05 +0200

    Refactor ClustersTable
    Move constants out of the component.

--- src/components/clusters/ClustersTable.tsx
+++ src/components/clusters/ClustersTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table, TableHeader, TableBody, TableVariant, IRowData } from '@patternfly/react-table';
+import { Table, TableHeader, TableBody, IRowData, sortable } from '@patternfly/react-table';
 import { ExtraParamsType } from '@patternfly/react-table/dist/js/components/Table/base/types';
 import { ClusterTableRows } from '../../types/clusters';
 
@@ -10,45 +10,48 @@ interface ClustersTableProps {
   deleteCluster: (id: string) => void;
 }
 
+// TODO(mlibra): let's try to remove it - not needed for HostsTable
+// TODO(jtomasek): Those should not be needed to define as they are optional,
+// needs fixing in @patternfly/react-table
+const headerStyle = {
+  position: 'sticky',
+  top: 0,
+  background: 'white',
+  zIndex: 1,
+};
+
+const columnConfig = {
+  transforms: [sortable],
+  cellTransforms: [],
+  formatters: [],
+  cellFormatters: [],
+  props: {},
+};
+
+const headerConfig = { header: { props: { style: headerStyle } } };
+
+const columns = [
+  { title: 'Name', ...headerConfig, ...columnConfig },
+  { title: 'ID', ...headerConfig, ...columnConfig },
+  { title: 'Version', ...headerConfig, ...columnConfig },
+  { title: 'Status', ...headerConfig, ...columnConfig },
+  { title: 'Hosts', ...headerConfig, ...columnConfig },
+];
+
 const ClustersTable: React.FC<ClustersTableProps> = ({ rows, deleteCluster }) => {
-  const headerStyle = {
-    position: 'sticky',
-    top: 0,
-    background: 'white',
-    zIndex: 1,
-  };
-  const headerConfig = { header: { props: { style: headerStyle } } };
-  // TODO(jtomasek): Those should not be needed to define as they are optional,
-  // needs fixing in @patternfly/react-table
-  const columnConfig = {
-    transforms: [],
-    cellTransforms: [],
-    formatters: [],
-    cellFormatters: [],
-    props: {},
-  };
-  const columns = [
-    { title: 'Name', ...headerConfig, ...columnConfig },
-    { title: 'ID', ...headerConfig, ...columnConfig },
-    { title: 'Version', ...headerConfig, ...columnConfig },
-    { title: 'Status', ...headerConfig, ...columnConfig },
-    { title: 'Hosts', ...headerConfig, ...columnConfig },
-  ];
-  const actions = [
-    {
-      title: 'Delete',
-      onClick: (event: React.MouseEvent, rowIndex: number, rowData: IRowData) =>
-        deleteCluster(rowData.id.title),
-    },
-  ];
+  const actions = React.useMemo(
+    () => [
+      {
+        title: 'Delete',
+        onClick: (event: React.MouseEvent, rowIndex: number, rowData: IRowData) =>
+          deleteCluster(rowData.id.title),
+      },
+    ],
+    [deleteCluster],
+  );
+
   return (
-    <Table
-      rows={rows}
-      cells={columns}
-      actions={actions}
-      variant={rows.length > 5 ? TableVariant.compact : undefined}
-      aria-label="Clusters table"
-    >
+    <Table rows={rows} cells={columns} actions={actions} aria-label="Clusters table">
       <TableHeader />
       <TableBody rowKey={rowKey} />
     </Table>
