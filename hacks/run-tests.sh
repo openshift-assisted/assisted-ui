6d27bd5afe621dabcfe9ed194d9c2a1ba7c5b046
Marek Libra <marek.libra@gmail.com>
2020-05-26 10:51:59 +0200

    Fix test image name

--- hacks/run-tests.sh
+++ hacks/run-tests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 CYPRESS_BASE_URL=${CYPRESS_BASE_URL:-http://192.168.0.126:3000} # URL of running Metal3 Installer UI
-TEST_IMAGE=${TEST_IMAGE:-"quay.io/mareklibra/facet-tests:123"}
+TEST_IMAGE=${TEST_IMAGE:-"quay.io/ocpmetal/ocp-metal-ui-tests:latest"}
 BASE_DIR=${BASE_DIR:-"`pwd`"/`date +%D_%T|sed 's/\//_/g'|sed 's/:/-/g'`}
+CONTAINER_COMMAND=${CONTAINER_COMMAND:-podman}
 
 echo Connecting to UI at: ${CYPRESS_BASE_URL}
 echo Test image: ${TEST_IMAGE}
@@ -12,7 +13,7 @@ SCREENSHOT_DIR=${BASE_DIR}/screenshots
 
 mkdir -p ${VIDEO_DIR}
 mkdir -p ${SCREENSHOT_DIR}
-docker run -it \
+${CONTAINER_COMMAND} run -it \
   -w /e2e \
   -e CYPRESS_BASE_URL="${CYPRESS_BASE_URL}" \
   --mount type=bind,source=${VIDEO_DIR},target=/e2e/cypress/videos \
