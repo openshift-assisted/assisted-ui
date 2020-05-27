8dd2944032ccc1dae9d0abea0576fdae324af4af
Marek Libra <marek.libra@gmail.com>
2020-05-27 10:33:18 +0200

    Fix typo

--- hacks/run-tests.sh
+++ hacks/run-tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 CYPRESS_BASE_URL=${CYPRESS_BASE_URL:-http://192.168.0.126:3000} # URL of running Metal3 Installer UI
-TEST_IMAGE=${TEST_IMAGE:-"quay.io/ocpmetal/ocp-metal-ui-tests:latest"}
+TESTS_IMAGE=${TESTS_IMAGE:-"quay.io/ocpmetal/ocp-metal-ui-tests:latest"}
 BASE_DIR=${BASE_DIR:-"`pwd`"/`date +%D_%T|sed 's/\//_/g'|sed 's/:/-/g'`}
 CONTAINER_COMMAND=${CONTAINER_COMMAND:-podman}
 
 echo Connecting to UI at: ${CYPRESS_BASE_URL}
-echo Test image: ${TEST_IMAGE}
+echo Test image: ${TESTS_IMAGE}
 
 VIDEO_DIR=${BASE_DIR}/videos
 SCREENSHOT_DIR=${BASE_DIR}/screenshots
@@ -18,7 +18,7 @@ ${CONTAINER_COMMAND} run -it \
   -e CYPRESS_BASE_URL="${CYPRESS_BASE_URL}" \
   --mount type=bind,source=${VIDEO_DIR},target=/e2e/cypress/videos \
   --mount type=bind,source=${SCREENSHOT_DIR},target=/e2e/cypress/screenshots \
-   "${TEST_IMAGE}"
+   "${TESTS_IMAGE}"
 
 echo Screenshots and videos can be found in ${BASE_DIR}
 
