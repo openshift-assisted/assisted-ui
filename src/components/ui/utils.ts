27856bf555f29909aff154db4b79fc7a5158364e
Rastislav Wagner <rawagner@redhat.com>
2020-05-28 09:36:30 +0200

    Remove usage of install_completed_at and install_started_at

--- src/components/ui/utils.ts
+++
@@ -1,8 +0,0 @@
-import { DASH } from '../constants';
-
-export const getHumanizedDateTime = (dateTime?: string) => {
-  if (!dateTime) return DASH;
-
-  const date = new Date(Date.parse(dateTime));
-  return date.toLocaleString();
-};
