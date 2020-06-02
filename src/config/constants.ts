7b42232a06a5be37fc05982c83df8491b4b8c828
Marek Libra <marek.libra@gmail.com>
2020-06-02 12:38:19 +0200

    Change event polling interval

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -14,7 +14,8 @@ export const OPENSHIFT_VERSION_OPTIONS: OpenshiftVersionOptionType[] = [
 export const CLUSTER_MANAGER_SITE_LINK =
   'https://cloud.redhat.com/openshift/install/metal/user-provisioned';
 
-export const POLLING_INTERVAL = 10000;
+export const POLLING_INTERVAL = 10 * 1000;
+export const EVENTS_POLLING_INTERVAL = 60 * 1000;
 
 export const HOST_ROLES = ['worker', 'master'];
 // Without undefined. Otherwise must conform generated Host['roles'] - see api/types.ts
