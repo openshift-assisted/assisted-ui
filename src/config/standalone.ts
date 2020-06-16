6fb15c4eeb81b323f1148e2089e1a701812c09fa
Jiri Tomasek <jtomasek@redhat.com>
2020-06-16 14:42:37 +0200

    Add API versions in AboutModal

--- src/config/standalone.ts
+++ src/config/standalone.ts
@@ -1,2 +1,10 @@
 export const VERSION = process.env.REACT_APP_VERSION;
 export const GIT_SHA = process.env.REACT_APP_GIT_SHA;
+
+export const SERVICE_LABELS: { [key in string]: string } = {
+  'assisted-installer': 'Assisted Installer',
+  'assisted-installer-service': 'Assisted Installer Service',
+  'discovery-agent': 'Discovery Agent',
+  'ignition-manifests-and-kubeconfig-generate': 'Ignition Manifests and Kubeconfig Generate',
+  'image-builder': 'Image Builder',
+};
