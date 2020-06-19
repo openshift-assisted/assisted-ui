4a5355a95259f703530644835554ede0272ae14b
Marek Libra <marek.libra@gmail.com>
2020-06-19 13:19:55 +0200

    Fix for GitHub workflow

--- hacks/patch-react-scripts.sh
+++ hacks/patch-react-scripts.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Fixes issue with watching changes to node_modules/facet_lib by webpack dev server
 sed -i 's/ ignored: ignoredFiles/ \/\* See hacks\/patch-react-scripts.sh \*\/ \/\/ ignored: ignoredFiles/g' node_modules/react-scripts/config/webpackDevServer.config.js 
 
