a9580d302e59d7bc2ac1de0cb1e384bea6854e99
Jonathan Kilzi <jkilzi@redhat.com>
2022-02-13 21:54:55 +0200

    chore: update tools for custom UI image

--- hacks/build_image.sh
+++ hacks/build_image.sh
@@ -1,24 +1,40 @@
 #!/usr/bin/env bash
 set -e
 
+DOCKERFILE="./Dockerfile"
+REACT_APP_BUILD_MODE=""
+
 usage() {
-  echo "Usage: $0 [ single-cluster ]" 1>&2
+cat << EOF
+This command must be executed from the root of the repository
+Usage: $0 [-f <dockerfile>] [-s|--single-cluster]
+EOF
 }
 
 parse_args() {
-  while getopts ":h" opt; do
+  while getopts ":f:sh" opt; do
     case $opt in
       h)
         usage
         exit 0
         ;;
+      s)
+        REACT_APP_BUILD_MODE="single-cluster"
+        ;;
+      f)
+        DOCKERFILE="$OPTARG"
+        ;;
       \?)
-        echo "Invalid option: -$OPTARG" >&2
+        echo "Invalid option: -${opt}" >&2
         usage
         exit 1
         ;;
       :)
-        echo "Option -$OPTARG requires an argument." >&2
+        echo "Option -$opt requires an argument." >&2
+        usage
+        exit 1
+        ;;
+      *)
         usage
         exit 1
         ;;
@@ -33,12 +49,23 @@ find_tool() {
 main() {
   parse_args "$@"
   PODMAN=$(find_tool)
+
+  pushd ../assisted-ui-lib
+  REACT_APP_GIT_SHA="$(git rev-parse --short HEAD)"
+  popd
+  REACT_APP_VERSION="$(git rev-parse --short HEAD)"
+
+  if [ "$DOCKERFILE" != './Dockerfile' ]; then
+    yarn build
+  fi
+
   ${PODMAN} build \
+    -f "$DOCKERFILE" \
     --force-rm \
-    --tag assisted-installer-ui:"${1:-local}" \
-    --build-arg=REACT_APP_BUILD_MODE="$1" \
-    --build-arg=REACT_APP_GIT_SHA="$(git rev-parse HEAD)" \
-    --build-arg=REACT_APP_VERSION=latest \
+    --tag quay.io/"$(whoami)"/assisted-installer-ui:"$REACT_APP_GIT_SHA" \
+    --build-arg=REACT_APP_BUILD_MODE="$REACT_APP_BUILD_MODE" \
+    --build-arg=REACT_APP_GIT_SHA="$REACT_APP_GIT_SHA" \
+    --build-arg=REACT_APP_VERSION="$REACT_APP_VERSION" \
     .
 }
 
