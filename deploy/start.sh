4882d67e96ba0c4ddf86c94ef1f83f65a7472833
Lev Veyde <lveyde@redhat.com>
2022-02-22 12:47:13 +0200

    Fixed the failure on systems with IPv6 being disabled (#538)
    Signed-off-by: Lev Veyde <lveyde@redhat.com>

--- deploy/start.sh
+++ deploy/start.sh
@@ -4,4 +4,10 @@ export ASSISTED_SERVICE_URL="${ASSISTED_SERVICE_URL:-http://localhost:8090}"
 
 envsubst '$ASSISTED_SERVICE_URL' < /deploy/nginx.conf > "$NGINX_DEFAULT_CONF_PATH/nginx.conf"
 
+# Do not listen on IPv6 if it's not enabled in the hardware
+if grep 'ipv6.disable=1' /proc/cmdline; then
+    cp "${NGINX_CONF_PATH}" ~/nginx.conf.orig
+    sed -e 's/listen\s*\[::\].*//g' ~/nginx.conf.orig | dd of="${NGINX_CONF_PATH}"
+fi
+
 exec nginx -g "daemon off;"
