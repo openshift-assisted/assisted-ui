248bed574599fc8c37debf7d418c66e5ec579baa
Jiri Tomasek <jtomasek@redhat.com>
2022-06-30 10:42:23 +0200

    Fix translations import & cleanup
    - remove unused dependency
    - remove package-lock.json since assisted-ui is using yarn
    - remove no longer needed translations

--- src/i18n.js
+++ src/i18n.js
@@ -1,49 +1,30 @@
-/* Copyright Contributors to the Open Cluster Management project */
-
-/* istanbul ignore file */
-
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import translation from './locales/en/translation.json';
-/** Localization */
-// the translations
-// the translations
-const resources = {
-  en: {
-    /* eslint-disable @typescript-eslint/camelcase */
-    assisted_installer: translation,
-  },
-};
+import enTranslation from 'openshift-assisted-ui-lib/dist/locales/en/translation.json';
 
-i18n
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  /// detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    resources,
-    supportedLngs: ['en'],
-    fallbackLng: 'en',
-    load: 'languageOnly',
-    detection: { caches: [] },
-    contextSeparator: '~',
-    defaultNS: 'translation',
-    nsSeparator: '~',
-    keySeparator: false,
-    debug: true,
-    react: {
-      useSuspense: true,
-      transSupportBasicHtmlNodes: true, // allow <br/> and simple html elements in translations
+i18n.use(initReactI18next).init({
+  lng: 'en',
+  resources: {
+    en: {
+      translation: enTranslation,
     },
-    missingKeyHandler: function (lng, ns, key) {
-      window.windowError = `Missing i18n key "${key}" in namespace "${ns}" and language "${lng}."`;
-      // eslint-disable-next-line no-console
-      console.error(window.windowError);
-    },
-  });
+  },
+  supportedLngs: ['en'],
+  fallbackLng: 'en',
+  load: 'languageOnly',
+  detection: { caches: [] },
+  defaultNS: 'translation',
+  nsSeparator: '~',
+  keySeparator: false,
+  debug: false,
+  react: {
+    useSuspense: true,
+    transSupportBasicHtmlNodes: true, // allow <br/> and simple html elements in translations
+  },
+  missingKeyHandler: function (lng, ns, key) {
+    window.windowError = `Missing i18n key "${key}" in namespace "${ns}" and language "${lng}."`;
+    console.error(window.windowError);
+  },
+});
 
 export default i18n;
